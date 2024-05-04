674405bcf08b279340452d94c01b1d45cf2ab000
ZiWang1105 <ziwang2@andrew.cmu.edu>
2024-05-04 14:33:42 -0400

    11

--- train_mipnerf360.sh
+++ train_mipnerf360.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python3 run.py --ginc configs/mipnerf360/carla.gin  --scene output_clear_world_with_depth
+CUDA_VISIBLE_DEVICES=2 python3 run.py --ginc configs/mipnerf360/carla.gin  --scene output_clear_world_with_depth
